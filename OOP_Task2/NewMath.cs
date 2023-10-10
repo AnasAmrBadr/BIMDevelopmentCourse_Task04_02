1dace2dc5107623dcab3c9b259542869779218b8
Anas Badr <anasamrbadr2@gmail.com>
2023-10-10 07:03:40 +0100

    Add project files.

---
+++ OOP_Task2/NewMath.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task2
+{
+    public static class NewMath
+    {
+
+        //Power(2)
+        public static double Square (double theBase)
+        {
+            double result = (theBase * theBase);
+            return result;
+        }
+
+        //Power(n)
+        public static double PowerN(double theBase, double thepower)
+        {
+            if (thepower % 1 != 0)
+            {
+                throw new Exception("ERROR: The power must be integer");
+            }
+
+            double theResult = 1;
+
+            for (int i = 0; i <= thepower; i++) 
+            {
+                theResult *= theBase;
+            }
+            return theResult;
+
+        }
+
+
+
+        //MaxNumber
+        public static double MaxNubmer(double numberOne, double numberTwo)
+        {
+            double maxNumber = 0;
+
+            if (numberOne > numberTwo)
+            {
+                 maxNumber = numberOne;
+            }
+            else
+            {
+                maxNumber = numberTwo;
+            }
+            return maxNumber;
+        }
+
+
+        //MinNumber
+        public static double MinNubmer(double numberOne, double numberTwo)
+        {
+            double minNumber = 0;
+
+            if (numberOne < numberTwo)
+            {
+                minNumber = numberOne;
+            }
+            else
+            {
+                minNumber = numberTwo;
+            }
+            return minNumber;
+        }
+
+        //Absolute
+
+        public static double Abso (double theNumber) 
+        { 
+            if (theNumber < 0) 
+            {
+                theNumber = -1 * theNumber;
+
+            }
+            return theNumber;
+        
+        }
+    }
+}
---
+++ OOP_Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 2
+            //Create a static class called Math
+            //The class has the following methods :
+            //Power(2) – Power(n) – SquareRoot – MaxNumber(takes two numbers returns the biggest) – MinNumber
+            //Absolute
+            //Use google search on how to create these methods mathematically
+
+            Console.WriteLine(NewMath.Square(2));
+            Console.WriteLine(NewMath.PowerN(3,4));
+            Console.WriteLine(NewMath.MaxNubmer(1,2));
+            Console.WriteLine(NewMath.MinNubmer(1, 2));
+            Console.WriteLine(NewMath.Abso(5));
+
+
+        }
+    }
+}
